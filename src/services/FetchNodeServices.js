25955386b817d684d93f101f545236f4763c8a3e
Sid-dev19 <shivsid17@gmail.com>
2025-12-23 17:17:07 +0530

    fix: connect react frontend to render backend and stabilize api handling

--- src/services/FetchNodeServices.js
+++ src/services/FetchNodeServices.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const serverURL = 'http://localhost:5000'
+const serverURL = "https://jobspider-backend.onrender.com";
 const token=localStorage.getItem('TOKEN')
 const config = {
     headers: { Authorization: `${token}` }
